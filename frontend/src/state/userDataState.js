ea03b0afc4376c771bbba41b180d71364c0be2ae
its-Saab <musaababs@gmail.com>
2021-04-27 22:24:34 +0200

    Add groupDataState - postDataState

--- frontend/src/state/userDataState.js
+++ frontend/src/state/userDataState.js
@@ -1,6 +1,23 @@
 import { atom } from "recoil";
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist()
 
 export const userDataState = atom({
   key: "userData",
   default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+
+export const groupDataState = atom({
+  key: "groupData",
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const postDataState = atom({
+  key: "postData",
+  default: [],
+  effects_UNSTABLE: [persistAtom],
 });
