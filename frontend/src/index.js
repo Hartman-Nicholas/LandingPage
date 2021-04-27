b453cec727df5fdc96b84d1f1be1544ed5219840
its-Saab <musaababs@gmail.com>
2021-04-27 22:48:48 +0200

    Add default locale for react-time-ago

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
 import "./styles/App.css";
+
+
+TimeAgo.addDefaultLocale(en)
 
 ReactDOM.render(<App />, document.getElementById("root"));
