3ebea86c76b2696f2fc46829666ff93d58d236d8
its-Saab <musaababs@gmail.com>
2021-04-28 00:17:26 +0200

    Update requests to be more consistent

--- frontend/src/api/CommentsApi.js
+++ frontend/src/api/CommentsApi.js
@@ -2,19 +2,19 @@ import Api from "./Api";
 
 class CommentsApi {
   getCommentByPostId(postId) {
-    return Api.get("/comments/" + postId);
+    return Api.get(`/comments/${postId}`);
   }
 
   createComment(postId, comment) {
-    return Api.post("/comments/" + postId, comment);
+    return Api.post(`/comments/${postId}`, comment);
   }
 
   updateComment(commentId, comment) {
-    return Api.put("/comments/" + commentId, comment);
+    return Api.put(`/comments/${commentId}`, comment);
   }
 
   deleteComment(commentId) {
-    return Api.delete("/comments/" + commentId);
+    return Api.delete(`/comments/${commentId}`);
   }
 }
 
