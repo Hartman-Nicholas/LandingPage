b9a0bb60b5cce08a4ea553c9ddec50c38d0166b8
its-Saab <musaababs@gmail.com>
2021-04-27 22:30:36 +0200

    Add comment card view

---
+++ frontend/src/components/comment/CommentCard.jsx
@@ -0,0 +1,20 @@
+// NPM Packages
+import ReactTimeAgo from "react-time-ago";
+
+// Project files
+
+export const CommentCard = ({ data }) => {
+	// State
+
+	// Constants
+
+	// Components
+	console.log("commentCard", data);
+	return (
+		<div>
+			<h3>{data.body}</h3>
+			<h3>By: {data.userCommentOwner}</h3>
+			Created: <ReactTimeAgo date={new Date(data.created)} locale="en-US" />
+		</div>
+	);
+};
