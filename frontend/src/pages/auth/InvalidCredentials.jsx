d192447de100e8d88a5bf16504996f535622031e
its-Saab <musaababs@gmail.com>
2021-04-28 16:08:30 +0200

    Add username check

---
+++ frontend/src/pages/auth/InvalidCredentials.jsx
@@ -0,0 +1,5 @@
+
+
+export const InvalidCredentials = () => {
+	return <h4 style={{color: "red"}}> Couldn't register check credentials and try again</h4>;
+};
--- frontend/src/pages/auth/RegisterForm.jsx
+++ frontend/src/pages/auth/RegisterForm.jsx
@@ -1,59 +1,78 @@
 import React, { useState } from "react";
 
 import Auth from "../../services/Auth";
+import UserApi from "../../api/UserApi";
+import {InvalidCredentials} from './InvalidCredentials'
 
 export default function RegisterForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [isSuccessful, setIsSuccessful] = useState(true);
 
-  async function register(registrationData) {
-    const registerSuccess = await Auth.register(registrationData);
-    if (!registerSuccess) {
-      alert("Couldn't register check credentials and try again");
-    }
-  }
+	async function register(registrationData) {
+		const registerSuccess = await Auth.register(registrationData);
+		if (!registerSuccess) {
+      setIsSuccessful(false)
+		}
+	}
 
-  return (
-    <div>
-      <h2>Sign up</h2>
-      <div>
-        <div>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Name"
-          />
-        </div>
+	// const checkUsername = async (username) => {
+	// 	UserApi.userNameExists(username)
+	// 		.then((res) => setDoesUsernameExists(res))
+	// 		.catch((err) => console.log(err));
+	// };
 
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-          />
-        </div>
+	const handleSubmit = (e) => {
+		// checkUsername(name);
 
-        <div>
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+		// if (!doesUsernameExists) {
+			return register({ name, email, password });
+		// } else {
+		// 	alert("username already exists");
+		// }
+	};
 
-        <div>
-          <button
-            type="submit"
-            onClick={(e) => register({ name, email, password })}
-          >
-            Create account
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Sign up</h2>
+			<div>
+				<div>
+					<input
+						type="text"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						placeholder="Name"
+					/>
+				</div>
+
+				<div>
+					<input
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="Email"
+					/>
+				</div>
+
+				<div>
+					<input
+						type="password"
+						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+
+				<div>
+          {!isSuccessful &&
+          <InvalidCredentials/>
+          }
+					<button type="submit" onClick={handleSubmit}>
+						Create account
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
