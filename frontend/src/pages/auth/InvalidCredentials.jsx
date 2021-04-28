719553450511f999a0a4c85bf8a2bcc671d7b5e8
its-Saab <musaababs@gmail.com>
2021-04-28 17:53:39 +0200

    Provide error messages for RegisterForm

--- frontend/src/pages/auth/InvalidCredentials.jsx
+++ frontend/src/pages/auth/InvalidCredentials.jsx
@@ -3,3 +3,9 @@
 export const InvalidCredentials = () => {
 	return <h4 style={{color: "red"}}> Couldn't register check credentials and try again</h4>;
 };
+
+
+export const UserExists = () => {
+	return <h4 style={{color: "red"}}>Username already exists please check your credentials</h4>;
+
+}
