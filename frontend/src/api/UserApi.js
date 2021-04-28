00bdce05ad186beb55df94e44473a6edeeb1c4e9
its-Saab <musaababs@gmail.com>
2021-04-28 15:43:08 +0200

    Add users api

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -4,6 +4,22 @@ class UserApi {
   getUser() {
     return Api.get("/users");
   }
+
+  userNameExists(username){
+    return Api.get(`/users/${username}`)
+  }
+
+  isFirstLogIn(){
+    return Api.post("/users")
+  }
+  updateUser(body){
+    return Api.put("/users", body)
+  }
+
+  //To be implemented
+  // deleteUser(userId){
+  //   return Api.delete(`/users/${userId}`)
+  // }
 }
 
 export default new UserApi();
