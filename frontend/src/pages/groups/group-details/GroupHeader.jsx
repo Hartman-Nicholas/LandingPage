b2e9302bd3e9edb2cbdecd49e837f35d67c1bdf0
its-Saab <musaababs@gmail.com>
2021-04-30 08:42:50 +0200

    Update GroupHeader to contain all sub-routes

--- frontend/src/pages/groups/group-details/GroupHeader.jsx
+++ frontend/src/pages/groups/group-details/GroupHeader.jsx
@@ -1,9 +1,14 @@
 // NPM Packages
-import {Link} from 'react-router-dom'
-import { About } from './About'
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 // Project files
+import { About } from "./About";
+import { Discussion } from "./Discussion";
+import { Members } from "./Members";
+import { ErrorMessage } from "../../../components/ErrorMessage";
 
-export const GroupHeader = ({groupId}) => {
+export const GroupHeader = ({ groupId, group }) => {
 	// State
 
 	// Constants
@@ -11,22 +16,49 @@ export const GroupHeader = ({groupId}) => {
 	// Components
 
 	return (
-		<nav>
-			<div>
-				<ul>
-					<li>
-						<Link to={`/groups/${groupId}/about`}>About</Link>
-					</li>
+		<Router>
+			<nav>
+				<div>
+					<ul>
+						<li>
+							<Link to={`/groups/${groupId}/about`}>About</Link>
+						</li>
 
-					<li>
-						<Link to={`/groups/${groupId}/discussion`}>Discussion</Link>
-					</li>
+						<li>
+							<Link to={`/groups/${groupId}/discussion`}>Discussion</Link>
+						</li>
 
-					<li>
-						<Link to={`/groups/${groupId}/members`}>Members</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
+						<li>
+							<Link to={`/groups/${groupId}/members`}>Members</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<ErrorBoundary FallbackComponent={ErrorMessage}>
+				<Suspense fallback={<div>loading...</div>}>
+					<Switch>
+						<Route
+							path="/groups/:id/about"
+							exact
+							render={(props) => (
+								<About key={props.match.params.id} groupData={group} />
+							)}
+						/>
+						<Route
+							path="/groups/:id/discussion"
+							exact
+							render={(props) => <Discussion key={props.match.params.id} />}
+						/>
+						<Route
+							path="/groups/:id/members"
+							exact
+							render={(props) => (
+								<Members key={props.match.params.id} groupData={group} />
+							)}
+						/>
+					</Switch>
+				</Suspense>
+			</ErrorBoundary>
+		</Router>
 	);
 };
