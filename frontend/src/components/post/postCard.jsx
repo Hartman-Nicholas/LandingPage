dc0b41cd1b638d827061e062d4fb4da6561e9974
its-Saab <musaababs@gmail.com>
2021-05-01 13:29:23 +0200

    Add comment submit handling

--- frontend/src/components/post/postCard.jsx
+++ frontend/src/components/post/postCard.jsx
@@ -1,32 +1,40 @@
 // NPM Packages
+import { useState, useEffect } from "react";
 import ReactTimeAgo from "react-time-ago";
 // Project files
 import { CommentCard } from "../comment/CommentCard";
 import { CommentForm } from "../comment/CommentForm";
 
 export const PostCard = ({ data }) => {
-  // State
-  // Constants
+	// State
+	const [commentsData, setCommentsData] = useState(data.comments);
 
-  // Components
-  return (
-    <div>
-      <h1>{data.body}</h1>
-      {data.length === 0 ? (
-        "No posts yet"
-      ) : (
-        <div>
-          <h3>{data.body}</h3>
-          <h3>{data.postOwner}</h3>
-          Created: <ReactTimeAgo date={new Date(data.created)} locale="en-US" />
-          {data.comments === null
-            ? "No comments"
-            : data.comments.map((comment) => {
-                return <CommentCard key={comment.id} data={comment} />;
-              })}
-          <CommentForm postId={data.id} />
-        </div>
-      )}
-    </div>
-  );
+	useEffect(() => {
+		setCommentsData(data.comments ? data.comments : []);
+	}, [data.comments]);
+
+	// Constants
+	const handleSubmit = (newComment) => {
+		const list = commentsData.concat(newComment);
+		setCommentsData(list);
+	};
+
+	// Components
+
+	let commentList =
+		(commentsData === null || commentsData.length === 0)
+			? "No Available comments"
+			: commentsData?.map((comment) => (
+					<CommentCard key={comment.id} data={comment} />
+			  ));
+	return (
+		<div>
+			{/* TODO update the return so it doesnt have duplicated data */}
+			<h1>{data.body}</h1>
+			<h3>{data.postOwner}</h3>
+			Created: <ReactTimeAgo date={new Date(data.created)} locale="en-US" />
+			{commentList}
+			<CommentForm postId={data.id} onSubmit={handleSubmit} />
+		</div>
+	);
 };
