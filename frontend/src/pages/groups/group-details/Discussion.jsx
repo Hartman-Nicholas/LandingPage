54552210dc01f605f53cd81e77de0c3b21901e5c
its-Saab <musaababs@gmail.com>
2021-05-01 13:29:02 +0200

    Debugg ternary operator

--- frontend/src/pages/groups/group-details/Discussion.jsx
+++ frontend/src/pages/groups/group-details/Discussion.jsx
@@ -6,31 +6,31 @@ import { PostCard } from "../../../components/post/postCard";
 import { PostForm } from "../../../components/post/PostForm";
 
 export const Discussion = ({ data }) => {
-  // State
-  const [postData, setPostData] = useState(data.posts);
+	// State
+	const [postData, setPostData] = useState(data.posts);
 
-  useEffect(() => {
-    setPostData(data.posts);
-  }, [data.posts]);
+	useEffect(() => {
+		setPostData(data.posts);
+	}, [data.posts]);
 
-  // Constants
+	// Constants
 
-  const handleSubmit = (newPost) => {
-    const list = postData.concat(newPost);
-    setPostData(list);
-  };
+	const handleSubmit = (newPost) => {
+		const list = postData.concat(newPost);
+		setPostData(list);
+	};
 
-  // Components
-  let postsList =
-    postData === null
-      ? "No Available posts"
-      : postData?.map((post) => <PostCard key={post.id} data={post} />);
+	// Components
+	let postsList =
+		(postData === undefined || postData.length === 0)
+			? "No Available posts"
+			: postData?.map((post) => <PostCard key={post.id} data={post} />);
 
-  return (
-    <div>
-      <h1>Discussion</h1>
-      <PostForm groupId={data.id} onSubmit={handleSubmit} />
-      {postsList}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Discussion</h1>
+			<PostForm groupId={data.id} onSubmit={handleSubmit} />
+			{postsList}
+		</div>
+	);
 };
