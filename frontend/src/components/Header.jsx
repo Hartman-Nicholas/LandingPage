608f5b7a5bdcd49b02d6c3a10e97e5fa8d02d911
its-Saab <musaababs@gmail.com>
2021-05-03 19:25:46 +0200

    Update Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,46 @@
 // NPM Packages
-
+import { Link } from "react-router-dom";
 //Project files
+import logo from "../assets/logoLandingPage.png";
 
 export default function Header({ onLogout, loggedIn }) {
-  // Components
+	// Components
 
-  return (
-    <nav>
-      <div>
-        <ul>
-          <li>Template Header</li>
-          {loggedIn && (
-            <li>
-              <button type="button" onClick={onLogout}>
-                Logout
-              </button>
-            </li>
-          )}
-        </ul>
-      </div>
-    </nav>
-  );
+	return (
+		<nav>
+			<div>
+				<ul>
+					{loggedIn && (
+						<li>
+							<Link to="/">
+								<img
+									src={logo}
+									alt="Logo"
+									style={{
+										width: "70px",
+										height: "50px",
+										borderRadius: "8px",
+									}}
+								/>
+							</Link>
+							<ul>
+								<li>
+									<Link to="/">Home</Link>
+								</li>
+								<li>
+									<Link to="/groups">Groups</Link>
+								</li>
+								<li>
+									<Link to="/user">Profile</Link>
+								</li>
+							</ul>
+							<button type="button" onClick={onLogout}>
+								Logout
+							</button>
+						</li>
+					)}
+				</ul>
+			</div>
+		</nav>
+	);
 }
