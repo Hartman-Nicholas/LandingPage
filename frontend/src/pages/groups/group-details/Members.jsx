d121196487d8290aa34e7352d33e6c6bc4a28d59
its-Saab <musaababs@gmail.com>
2021-05-03 13:51:22 +0200

    Add Members page

--- frontend/src/pages/groups/group-details/Members.jsx
+++ frontend/src/pages/groups/group-details/Members.jsx
@@ -1,13 +1,26 @@
 // NPM Packages
+import { useState, useEffect } from "react";
+import { MemberCard } from "./MemberCard";
 
 // Project files
 
-export const Members = ({ data }) => {
-  // State
+export const Members = ({ data, status }) => {
+	// State
+	const [membersList, setMembersList] = useState(data?.members);
+console.log("membersList", membersList)
+	useEffect(() => {
+		setMembersList(data.members);
+	}, [status, data.id]);
+	// Constants
+	//TODO update after BE fixes the members array data
+	let list =
+		membersList.length === 0
+			? <h3>No available members</h3>
+			: membersList?.map((member) => (
+					<MemberCard key={member.id} member={member} />
+			  ));
 
-  // Constants
+	// Components
 
-  // Components
-
-  return <h1>Members List</h1>;
+	return <div>{list}</div>;
 };
