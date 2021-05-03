6a2fed64428fb9408dca07ac16f88cf839914838
its-Saab <musaababs@gmail.com>
2021-05-03 14:02:38 +0200

    Take out handle buttons to GroupHome

--- frontend/src/pages/groups/group-details/GroupHeader.jsx
+++ frontend/src/pages/groups/group-details/GroupHeader.jsx
@@ -1,54 +1,33 @@
 // NPM Packages
-import { useState } from "react";
+
 // Project files
 import { About } from "./About";
 import { Discussion } from "./Discussion";
 import { Members } from "./Members";
 
-export const GroupHeader = ({ group }) => {
-  // State
-  const [aboutState, setAboutState] = useState(false);
-  const [discussionState, setDiscussionState] = useState(true);
-  const [MembersState, setMembersState] = useState(false);
-  // Constants
+export const GroupHeader = ({
+	group,
+	handleSubmit,
+	aboutState,
+	discussionState,
+	membersState,
+}) => {
+	// State
 
-  // Components
-  const handleSubmit = (e) => {
-    switch (e.target.name) {
-      case "about":
-        setAboutState(true);
-        setDiscussionState(false);
-        setMembersState(false);
-        break;
-      case "discussion":
-        setAboutState(false);
-        setDiscussionState(true);
-        setMembersState(false);
-        break;
-      case "members":
-        setAboutState(false);
-        setDiscussionState(false);
-        setMembersState(true);
-        break;
-      default:
-        break;
-    }
-  };
-
-  return (
-    <div>
-      <button name="about" onClick={handleSubmit}>
-        About{" "}
-      </button>
-      <button name="discussion" onClick={handleSubmit}>
-        Discussion{" "}
-      </button>
-      <button name="members" onClick={handleSubmit}>
-        Members{" "}
-      </button>
-      {aboutState && <About data={group}  />}
-      {discussionState && <Discussion data={group} status={discussionState} />}
-      {MembersState && <Members data={group} />}
-    </div>
-  );
+	return (
+		<div>
+			<button name="about" onClick={handleSubmit}>
+				About{" "}
+			</button>
+			<button name="discussion" onClick={handleSubmit}>
+				Discussion{" "}
+			</button>
+			<button name="members" onClick={handleSubmit}>
+				Members{" "}
+			</button>
+			{aboutState && <About data={group} />}
+			{discussionState && <Discussion data={group} />}
+			{membersState && <Members data={group} />}
+		</div>
+	);
 };
