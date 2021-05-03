f1e7bd4a46da31efc8eb38b1eb2fcd68e5b7c619
its-Saab <musaababs@gmail.com>
2021-05-03 14:00:57 +0200

    Add default avatar to groupForm

--- frontend/src/pages/groups/GroupForm.jsx
+++ frontend/src/pages/groups/GroupForm.jsx
@@ -8,63 +8,66 @@ import GroupApi from "../../api/GroupApi";
 import UserApi from "../../api/UserApi";
 
 export const GroupForm = () => {
-  // State
-  const [groupForm, setGroupForm] = useState({
-    title: "",
-    description: "",
-  });
-  const [userData, setUserData] = useRecoilState(userDataState);
+	// State
+	const [groupForm, setGroupForm] = useState({
+		title: "",
+		description: "",
+		avatar:
+			"https://res.cloudinary.com/dlvwrtpzq/image/upload/v1619987659/profilePhotos/placeholder_eo6jkp.png",
+	});
+	const [userData, setUserData] = useRecoilState(userDataState);
 
-  // Constants
-  async function createGroup(requestBody) {
-    try {
-      await GroupApi.createGroup(requestBody);
-      // await GroupApi.getAllGroups().then(({ data }) => {
-      // 	setGroupData(data);
-      // });
-      await UserApi.getUser().then(({ data }) => setUserData(data));
-    } catch (e) {
-      console.error(e);
-    }
-  }
+	// Constants
+	async function createGroup(requestBody) {
+		try {
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setGroupForm({
-      ...groupForm,
-      [name]: value,
-    });
-  };
+			await GroupApi.createGroup(requestBody);
+			// await GroupApi.getAllGroups().then(({ data }) => {
+			// 	setGroupData(data);
+			// });
+			await UserApi.getUser().then(({ data }) => setUserData(data));
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    createGroup(groupForm);
-    setGroupForm({
-      title: "",
-      description: "",
-    });
-  };
-  // Components
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={groupForm.title}
-        onChange={handleChange}
-        placeholder="Group name"
-        type="text"
-        name="title"
-        required
-      />
-      <br />
-      <textarea
-        value={groupForm.description}
-        onChange={handleChange}
-        placeholder="Group description"
-        type="text"
-        name="description"
-        required
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setGroupForm({
+			...groupForm,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createGroup(groupForm);
+		setGroupForm({
+			title: "",
+			description: "",
+		});
+	};
+	// Components
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				value={groupForm.title}
+				onChange={handleChange}
+				placeholder="Group name"
+				type="text"
+				name="title"
+				required
+			/>
+			<br />
+			<textarea
+				value={groupForm.description}
+				onChange={handleChange}
+				placeholder="Group description"
+				type="text"
+				name="description"
+				required
+			/>
+			<button type="submit">Submit</button>
+		</form>
+	);
 };
