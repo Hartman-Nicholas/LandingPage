24cf2e641154e00660f2c581a22fb4bbc30c3abc
its-Saab <musaababs@gmail.com>
2021-05-04 13:25:23 +0200

    Update postCard to handle Update-Edit

--- frontend/src/components/post/postCard.jsx
+++ frontend/src/components/post/postCard.jsx
@@ -1,24 +1,34 @@
 // NPM Packages
 import { useState, useEffect } from "react";
+import {useRecoilValue} from 'recoil'
 import ReactTimeAgo from "react-time-ago";
 // Project files
 import { CommentCard } from "../comment/CommentCard";
 import { CommentForm } from "../comment/CommentForm";
+import {userDataState} from "../../state/userDataState"
+import { EditPostForm } from "./EditPostForm";
+
 
 export const PostCard = ({ data }) => {
 	// State
 	const [commentsData, setCommentsData] = useState(data.comments);
+	const [toggler, setToggler] = useState(false)
+	const user = useRecoilValue(userDataState);
 
 	useEffect(() => {
 		setCommentsData(data.comments ? data.comments : []);
 	}, [data.comments]);
-
+console.log(toggler)
 	// Constants
 	const handleSubmit = (newComment) => {
 		const list = commentsData.concat(newComment);
 		setCommentsData(list);
 	};
 
+	const handleUpdate = updatedPost => {
+		data.body = updatedPost;
+		setToggler(false);
+	}
 	// Components
 
 	let commentList =
@@ -27,14 +37,32 @@ export const PostCard = ({ data }) => {
 			: commentsData?.map((comment) => (
 					<CommentCard key={comment.id} data={comment} />
 			  ));
+
+
 	return (
 		<div>
 			{/* TODO update the return so it doesnt have duplicated data */}
-			<h1>{data.body}</h1>
-			<h3>{data.postOwner}</h3>
-			Created: <ReactTimeAgo date={new Date(data.created)} locale="en-US" />
-			{commentList}
-			<CommentForm postId={data.id} onSubmit={handleSubmit} />
+		{
+			!toggler &&
+			<div>
+				<h1>{data.body}</h1>
+				<h3>{data.postOwner}</h3>
+				Created: <ReactTimeAgo date={new Date(data.created)} locale="en-US" />
+				{data.postOwner === user.name &&
+				<>
+					<button onClick={()=> setToggler(true)}>Edit</button>
+					{commentList}
+					<CommentForm postId={data.id} onSubmit={handleSubmit} />
+				</>
+			}
+
+			</div>
+		}
+
+			{
+				toggler &&
+				<EditPostForm data={data} onSubmit={handleUpdate} />
+			}
 		</div>
 	);
 };
