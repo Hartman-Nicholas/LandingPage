ab42bb6884548704a0e2a21c67c3c132a16f72c6
its-Saab <musaababs@gmail.com>
2021-05-04 18:24:27 +0200

    Destructure data - Refactor Update Post

--- frontend/src/components/post/EditPostForm.jsx
+++ frontend/src/components/post/EditPostForm.jsx
@@ -4,54 +4,50 @@ import { useState } from "react";
 // Project files
 import PostApi from "../../api/PostsApi";
 
-export const EditPostForm = ({ data, onSubmit }) => {
+export const EditPostForm = ({ data, onSubmit, postId }) => {
+	// State
+	const [postForm, setPostForm] = useState({
+		body: data,
+	});
 
-  // State
-  const [postForm, setPostForm] = useState({
-    body: data.body,
-  });
+	// Constants
+	async function postUpdate(id, requestBody) {
+		try {
+			await PostApi.updatePost(id, requestBody).then(({ data: { body } }) =>
+				onSubmit(body)
+			);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setPostForm({
+			...postForm,
+			[name]: value,
+		});
+	};
 
-  // Constants
-  async function postUpdate(postId, requestBody) {
-    try {
-      console.log("requestBody",requestBody)
-      await PostApi.updatePost(postId, requestBody).then(({data}) =>
-        onSubmit(data.body)
-      );
-    } catch (e) {
-      console.error(e);
-    }
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		postUpdate(postId, postForm);
+	};
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPostForm({
-      ...postForm,
-      [name]: value,
-    });
-  };
+	// Components
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    postUpdate(data.id, postForm);
-  };
-
-
-  // Components
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <textarea
-        value={postForm.body}
-        onChange={handleChange}
-        placeholder="what's on your mind..."
-        type="text"
-        name="body"
-        required
-        maxLength="255"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit}>
+			<textarea
+				value={postForm.body}
+				onChange={handleChange}
+				placeholder="what's on your mind..."
+				type="text"
+				name="body"
+				required
+				maxLength="255"
+			/>
+			<button type="submit">Save</button>
+		</form>
+	);
 };
--- frontend/src/components/post/postCard.jsx
+++ frontend/src/components/post/postCard.jsx
@@ -1,23 +1,25 @@
 // NPM Packages
 import { useState, useEffect } from "react";
-import {useRecoilValue} from 'recoil'
+import { useRecoilValue } from "recoil";
 import ReactTimeAgo from "react-time-ago";
 // Project files
 import { CommentCard } from "../comment/CommentCard";
 import { CommentForm } from "../comment/CommentForm";
-import {userDataState} from "../../state/userDataState"
+import { userDataState } from "../../state/userDataState";
 import { EditPostForm } from "./EditPostForm";
 
-
-export const PostCard = ({ data }) => {
+export const PostCard = ({
+	data: { id, comments, body, postOwner, created },
+}) => {
 	// State
-	const [commentsData, setCommentsData] = useState(data.comments);
-	const [toggler, setToggler] = useState(false)
-	const {name: userInSession} = useRecoilValue(userDataState);
+	const [commentsData, setCommentsData] = useState(comments);
+	const [postBody, setPostBody] = useState(body);
+	const [toggler, setToggler] = useState(false);
+	const { name: userInSession } = useRecoilValue(userDataState);
 
 	useEffect(() => {
-		setCommentsData(data.comments ? data.comments : []);
-	}, [data.comments]);
+		setCommentsData(comments ? comments : []);
+	}, [comments]);
 
 	// Constants
 	const handleSubmit = (newComment) => {
@@ -25,41 +27,40 @@ export const PostCard = ({ data }) => {
 		setCommentsData(list);
 	};
 
-	const handleUpdate = updatedPost => {
-		data.body = updatedPost;
+	const handleUpdate = (updatedPost) => {
+		setPostBody(updatedPost);
 		setToggler(false);
-	}
+	};
 	// Components
 
 	let commentList =
-		(commentsData === null || commentsData.length === 0)
+		commentsData === null || commentsData.length === 0
 			? "No Available comments"
 			: commentsData?.map((comment) => (
 					<CommentCard key={comment.id} data={comment} />
 			  ));
-
-
 	return (
 		<div>
 			{/* TODO update the return so it doesnt have duplicated data */}
-		{
-			!toggler &&
-			<div>
-				<h1>{data.body}</h1>
-				<h3>{data.postOwner}</h3>
-				Created: <ReactTimeAgo date={new Date(data.created)} locale="en-US" />
-				{data.postOwner === userInSession &&
-					<button onClick={()=> setToggler(true)}>Edit</button>
-				}
+			{!toggler && (
+				<div>
+					<h1>{postBody}</h1>
+					<h3>{postOwner}</h3>
+					Created: <ReactTimeAgo date={new Date(created)} locale="en-US" />
+					{postOwner === userInSession && (
+						<button onClick={() => setToggler(true)}>Edit</button>
+					)}
 					{commentList}
-					<CommentForm postId={data.id} onSubmit={handleSubmit} />
-			</div>
-		}
+					<CommentForm postId={id} onSubmit={handleSubmit} />
+				</div>
+			)}
 
-			{
-				toggler &&
-				<EditPostForm data={data} onSubmit={handleUpdate} />
-			}
+			{toggler && (
+				<>
+					<EditPostForm data={postBody} onSubmit={handleUpdate} postId={id} />
+					<button onClick={() => setToggler(false)}>Close</button>
+				</>
+			)}
 		</div>
 	);
 };
