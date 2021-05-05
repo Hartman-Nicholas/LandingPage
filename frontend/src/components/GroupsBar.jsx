adc14bb395c6a015674859cdfcd61c74907e3d89
nh-g <giangngohong@gmail.com>
2021-05-05 22:51:58 +0200

    switch postion of join and create part in sidebar

--- frontend/src/components/GroupsBar.jsx
+++ frontend/src/components/GroupsBar.jsx
@@ -24,34 +24,31 @@ export const GroupsBar = () => {
     return <GroupCard key={group.id} groupData={group} />;
   });
 
-  const groupCreatedTitles = groupCreated.map((item)=> {
-    return (
-      <GroupCard key={item.id} title ={item.title}/>
-    )
-  }
-  )
+  const groupCreatedTitles = groupCreated.map((item) => {
+    return <GroupCard key={item.id} title={item.title} />;
+  });
 
   useEffect(() => {
     setUserData(data);
   }, []);
 
   return (
-    <section id="groupSideBar">
+    <section id="side-bar">
       <div className="sidebarWrapper">
         <button className="sidebarButtonTop">
-          <Link to="/groups/create">+ Create a group</Link>
+          <Link to="/groups">Join a new group +</Link>
         </button>
-        {/* <h2 className="sidebarTitle">Your Group</h2> */}
+        <h2>joined :</h2>
+        {groupsJoined}
+        <h2>created :</h2>
         <ul className="sidebarList">
           <li className="sidebarListItem">
             <span>{groupCreated}</span>
             {/* <span className="sidebarListItemText"> {groupCreated}</span> */}
           </li>
         </ul>
-        {/* <h2>Groups joined :</h2>
-        {groupsJoined} */}
         <button className="sidebarButtonBottom">
-          <Link to="/groups">Join a new group +</Link>
+          <Link to="/groups/create">+ Create a group</Link>
         </button>
       </div>
     </section>
