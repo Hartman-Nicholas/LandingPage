e19ed1d453e86c2b29c7989c3c91d364694fe42b
its-Saab <musaababs@gmail.com>
2021-05-05 22:01:05 +0200

    Add search bar

--- frontend/src/pages/groups/Groups.jsx
+++ frontend/src/pages/groups/Groups.jsx
@@ -1,5 +1,5 @@
 // NPM Packages
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 // Project files
 import { GroupCard } from "./GroupCard";
@@ -12,6 +12,13 @@ export const Groups = () => {
 	// State
 	const [groupsList, setGroupsList] = useState([]);
 	const [userData, setUserData] = useRecoilState(userDataState);
+	const [flag, setFlag] = useState(false)
+	const query = useRef({
+		title: "",
+		topics: [],
+	});
+	const [queryList, setQueryList] = useState([]);
+
 	// Constants
 	const addMember = async (groupId) => {
 		await GroupApi.joinGroup(groupId).then(({ data }) => {
@@ -31,22 +38,104 @@ export const Groups = () => {
 		groupList();
 	}, [userData.groupsJoined]);
 
+	// handle search bar:
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+
+		if (name === "title") {
+			query.current = { ...query.current, [name]: value };
+		} else {
+			if (query.current.topics.includes(name)) {
+				setFlag(false)
+				query.current = {
+					...query.current,
+					topics: query.current.topics.filter((topic) => topic !== name),
+				};
+			} else {
+				setFlag(true)
+				query.current = {
+					...query.current,
+					topics: [...query.current.topics, name],
+				};
+			}
+		}
+
+		let filteredList = groupsList.filter((group) => {
+			if (query.current.title === "") {
+				return group.topics.some(
+					(topic) => query.current.topics.indexOf(topic) >= 0
+				);
+			} else if (query.current.topics.length === 0) {
+				return group.title.toLowerCase().includes(query.current.title.toLowerCase());
+			} else {
+				return (
+					group.title.includes(query.current.title) &&
+					group.topics.some((topic) => query.current.topics.indexOf(topic) >= 0)
+				);
+			}
+		});
+		setQueryList(filteredList);
+	};
+
 	// Components
 
 	return (
 		<div>
 			<h1>All Groups</h1>
-
-			{groupsList.length === 0
-				? "No groups available"
-				: groupsList.map((group) => (
-						<GroupCard
-							key={group.id}
-							groupData={group}
-							joinGroup={(id) => addMember(id)}
-						/>
-				  ))}
-
+			<input name="title" onChange={handleChange} />
+			<label htmlFor="Sport"> Sport</label>
+			<input
+				onChange={(event) => handleChange(event)}
+				type="checkbox"
+				id="Sport"
+				name="Sport"
+			/>
+			<label htmlFor="Health"> Health</label>
+			<input
+				onChange={handleChange}
+				type="checkbox"
+				id="Health"
+				name="Health"
+			/>
+			<label htmlFor="Entertainment"> Entertainment</label>
+			<input
+				onChange={handleChange}
+				type="checkbox"
+				id="Entertainment"
+				name="Entertainment"
+			/>
+			<label htmlFor="Education"> Education</label>
+			<input
+				onChange={handleChange}
+				type="checkbox"
+				id="Education"
+				name="Education"
+			/>
+			<label htmlFor="Family"> Family</label>
+			<input
+				onChange={handleChange}
+				type="checkbox"
+				id="Family"
+				name="Family"
+			/>
+			<br />
+						{
+						queryList.length > 0 || flag ?
+						queryList.map((group) => (
+							<GroupCard
+								key={group.id}
+								groupData={group}
+								joinGroup={(id) => addMember(id)}
+							/>
+						)): groupsList.map((group) => (
+								<GroupCard
+									key={group.id}
+									groupData={group}
+									joinGroup={(id) => addMember(id)}
+								/>
+							))
+						}
+			<br />
 			<OwnerGroupsBar />
 		</div>
 	);
