d9de41f56a62a04c8bd0ea27b0170b1365998401
nh-g <giangngohong@gmail.com>
2021-05-07 13:44:32 +0200

    remove unuesed group bar import

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -2,7 +2,6 @@
 
 // Project files
 import Feed from "../../components/Feed";
-import { GroupsBar } from "../../components/GroupsBar";
 import UserPosts from "../../components/UserPosts";
 
 export default function Home() {
