67dcc78dca9b8bfae7a81d72d287d05373e1e3f6
nh-g <giangngohong@gmail.com>
2021-05-07 13:44:12 +0200

    use recoilstate to get userData to update it when creating new post. Remove duplicate array map

--- frontend/src/components/UserPosts.jsx
+++ frontend/src/components/UserPosts.jsx
@@ -1,21 +1,21 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { userDataState } from "../../src/state/userDataState";
 import { PostCard } from "./post/postCard";
 
 export default function UserPosts() {
-  const userData = useRecoilValue(userDataState);
+  const [userData, setUserData] = useRecoilState(userDataState);
+  
+  // console.log({data});
+  console.log("userData", userData);
 
-  const userPosts = userData?.posts?.map((post) =>
-            <PostCard data={post}/>)
-|| [];
+  const userPosts =
+    userData?.posts?.map((post) => <PostCard key = {post.id} data={post} />) || [];
 
   return (
     <div className="rightBar">
       <div className="rightBarWrapper">
         Right Bar Template
-        {userData?.posts?.map((post) => (
-          <PostCard data={post}/>
-        ))}
+        {userPosts}
       </div>
     </div>
   );
