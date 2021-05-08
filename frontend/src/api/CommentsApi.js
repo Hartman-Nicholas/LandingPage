fc5d2299c602c26b58e87f3dd03d1fe2ce50464f
Nicholas Hartman <hartn001@gmail.com>
2021-05-08 19:58:10 +0200

    Post Like and comment Like complete

--- frontend/src/api/CommentsApi.js
+++ frontend/src/api/CommentsApi.js
@@ -16,6 +16,31 @@ class CommentsApi {
   deleteComment(commentId) {
     return Api.delete(`/comments/${commentId}`);
   }
+  checkLikeComment(commentId) {
+    return Api.get(`/comments/${commentId}/likes/check`);
+  }
+
+  likeComment(commentId) {
+    return Api.post(`/comments/${commentId}/likes`, { commentLike: "like" });
+  }
+
+  deletelikeComment(likeId) {
+    return Api.delete(`/comments/likes/${likeId}`);
+  }
+
+  checkDislikeComment(commentId) {
+    return Api.get(`/comments/${commentId}/dislikes/check`);
+  }
+
+  dislikeComment(commentId) {
+    return Api.post(`/comments/${commentId}/dislikes`, {
+      commentDislike: "dislike",
+    });
+  }
+
+  deleteDislikeComment(dislikeId) {
+    return Api.delete(`/comments/dislikes/${dislikeId}`);
+  }
 }
 
 export default new CommentsApi();
