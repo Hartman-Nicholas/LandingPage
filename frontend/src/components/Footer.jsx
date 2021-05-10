1c517b5f0810cbacdb0e824b6b5155dc11c3e9ca
kavyaashwath <kavya.ranju@gmail.com>
2021-05-10 09:44:27 +0200

    added footer links and info

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,22 @@
-export default function Footer() {
+import { Link } from "react-router-dom";
+
+export default function Footer({ loggedIn }) {
   return (
-    <footer>
-      <div>Footer Template</div>
+    <footer className="foot-bar">
+      <div>
+        <h3>Write to us</h3>
+        <a href="mailto:communityimmigrant@.co.se">
+          {" "}
+          communityimmigrant@gmail.com{" "}
+        </a>
+        {loggedIn && (
+          <div>
+            <Link className="home-link" to="/groups">
+              <button className="home-button">Go to Your Groups</button>
+            </Link>
+          </div>
+        )}
+      </div>
     </footer>
   );
 }
