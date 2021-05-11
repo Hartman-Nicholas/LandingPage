3a370412c8432d7a839788672496cdae770c3bac
nh-g <giangngohong@gmail.com>
2021-05-11 06:16:34 +0200

    change edit button position

--- frontend/src/pages/user/UserCard.jsx
+++ frontend/src/pages/user/UserCard.jsx
@@ -4,11 +4,16 @@ import { userDataState } from "../../state/userDataState";
 
 import EditProfile from './EditProfile';
 export default function UserCard() {
-	const userData = useRecoilValue(userDataState);
+  // State
+  const userData = useRecoilValue(userDataState);
   const [toggler, setToggler] = useState(false);
 
   console.log({userData});
 
+  // Constants
+
+
+  // Components
 	return (
     <section>
       <h1>UserCard Template</h1>
@@ -18,27 +23,28 @@ export default function UserCard() {
       <div className="listItem">Name: {userData.name}</div>
       <div className="listItem">Email: {userData.email}</div>
       <div className="listItem">Bio: {userData.bio}</div>
-      <div>
-        <label htmlFor="">Created </label>
-        {userData?.groupsCreated?.length} groups
-      </div>
-      <div>
-        <label htmlFor="">Joined </label>
-        {userData?.groupsJoined?.length} groups
-      </div>
-      <div>
-        <label htmlFor="">Posts </label>
-        {userData?.posts?.length} posts
-      </div>
-      <div>
-        <label htmlFor="">Comments </label>
-        {userData?.comments?.length} comments
-      </div>
 
       <button onClick={() => setToggler(!toggler)}>Edit Profile</button>
       {toggler && (
-        <EditProfile currentUser={userData} />
+        <EditProfile currentUser={userData} setToggler={setToggler} />
       )}
+      
+      <div>
+        <label htmlFor="">Created </label>
+        {userData?.groupsCreated?.length} 
+      </div>
+      <div>
+        <label htmlFor="">Joined </label>
+        {userData?.groupsJoined?.length} 
+      </div>
+      <div>
+        <label htmlFor="">Posts </label>
+        {userData?.posts?.length} 
+      </div>
+      <div>
+        <label htmlFor="">Comments </label>
+        {userData?.comments?.length} 
+      </div>
     </section>
   );
 }
