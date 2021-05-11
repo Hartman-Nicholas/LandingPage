bc9739f6e385984d5d3fe6e26510963118d74fab
nh-g <giangngohong@gmail.com>
2021-05-11 08:55:27 +0200

    add imageupload component to edit avatar

--- frontend/src/pages/user/UserCard.jsx
+++ frontend/src/pages/user/UserCard.jsx
@@ -1,25 +1,50 @@
-import {useState} from 'react';
-import { useRecoilValue } from "recoil";
+import { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
 import { userDataState } from "../../state/userDataState";
 
-import EditProfile from './EditProfile';
+import EditProfile from "./EditProfile";
+import { ImageUploader } from "../../components/ImageUploader";
+import UserApi from "../../api/UserApi";
+
 export default function UserCard() {
   // State
-  const userData = useRecoilValue(userDataState);
+  const [userData, setUserData] = useRecoilState(userDataState);
+  const [imageUrl, setImageUrl] = useState(userData.avatar);
   const [toggler, setToggler] = useState(false);
 
-  console.log({userData});
+  const updateProfile = async (avatar) => {
+    try {
+      console.log("updateProfile", { avatar });
+      const response = await UserApi.updateUser({ avatar });
+      console.log({ response });
+      setUserData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!imageUrl) {
+      return;
+    }
+    console.log("useEffect for imageUrl", { imageUrl });
+    updateProfile(imageUrl);
+    console.log({ userData });
+  }, [imageUrl]);
+
+  console.log({ userData });
 
   // Constants
 
-
   // Components
-	return (
+  return (
     <section>
       <h1>UserCard Template</h1>
       <div className="listItem">
-        <img src={userData.avatar} alt="avatar" />
+        <img className="groupForm--avatar" src={userData.avatar} alt="avatar" />
       </div>
+      <ImageUploader setImageState={setImageUrl} />
+
       <div className="listItem">Name: {userData.name}</div>
       <div className="listItem">Email: {userData.email}</div>
       <div className="listItem">Bio: {userData.bio}</div>
@@ -28,22 +53,22 @@ export default function UserCard() {
       {toggler && (
         <EditProfile currentUser={userData} setToggler={setToggler} />
       )}
-      
+
       <div>
         <label htmlFor="">Created </label>
-        {userData?.groupsCreated?.length} 
+        {userData?.groupsCreated?.length}
       </div>
       <div>
         <label htmlFor="">Joined </label>
-        {userData?.groupsJoined?.length} 
+        {userData?.groupsJoined?.length}
       </div>
       <div>
         <label htmlFor="">Posts </label>
-        {userData?.posts?.length} 
+        {userData?.posts?.length}
       </div>
       <div>
         <label htmlFor="">Comments </label>
-        {userData?.comments?.length} 
+        {userData?.comments?.length}
       </div>
     </section>
   );
