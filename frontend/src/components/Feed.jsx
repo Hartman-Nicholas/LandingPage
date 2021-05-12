b8c562edb0d241a49739cd47fa4f3cd8930ac407
kavyaashwath <kavya.ranju@gmail.com>
2021-05-12 09:54:40 +0200

    added the landing page

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,41 +1,42 @@
 //NPM packages
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 //Project files
-import UserApi from '../api/UserApi'
-import PostsApi from '../api/PostsApi'
-import {PostCard} from '../components/post/postCard'
+import UserApi from "../api/UserApi";
+import PostsApi from "../api/PostsApi";
+import { PostCard } from "../components/post/postCard";
+import LandingPage from "../pages/home/LandingPage";
 
 export default function Feed() {
-const [userFeed, setUserFeed] = useState([])
-const [flag, setFlag] = useState(false)
+  const [userFeed, setUserFeed] = useState([]);
+  const [flag, setFlag] = useState(false);
 
-useEffect(() => {
-  const fetchUserFeed = async()=> {
-    await UserApi.userFeed().then(({data}) => setUserFeed(data))
-  }
-  fetchUserFeed()
-}, [flag])
+  useEffect(() => {
+    const fetchUserFeed = async () => {
+      await UserApi.userFeed().then(({ data }) => setUserFeed(data));
+    };
+    fetchUserFeed();
+  }, [flag]);
 
-const deletePost = async (postId) => {
-  try {
-    await PostsApi.deletePost(postId).then(()=>setFlag(true));
-  } catch (e) {
-    console.error(e);
-  }
-};
+  const deletePost = async (postId) => {
+    try {
+      await PostsApi.deletePost(postId).then(() => setFlag(true));
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-let stream =
-(userFeed === undefined || userFeed.length) === 0
-  ? "Join a new group to see some feed"
-  : userFeed?.map(feed => (
-    <PostCard key={feed.id} data={feed} handleDelete={deletePost} />
-  ))
-    return (
-      <div className="center">
-        <div className="centerWrapper">
-      {stream}
-        </div>
-      </div>
+  let stream =
+    (userFeed === undefined || userFeed.length) === 0 ? (
+      <LandingPage />
+    ) : (
+      userFeed?.map((feed) => (
+        <PostCard key={feed.id} data={feed} handleDelete={deletePost} />
+      ))
     );
+  return (
+    <div className="center">
+      <div className="centerWrapper">{stream}</div>
+    </div>
+  );
 }
