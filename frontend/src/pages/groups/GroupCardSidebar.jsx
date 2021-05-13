171ba72542f214905320b8dc74a12dd07efa0744
nh-g <giangngohong@gmail.com>
2021-05-13 07:03:29 +0200

    add background image constant to display in small device

--- frontend/src/pages/groups/GroupCardSidebar.jsx
+++ frontend/src/pages/groups/GroupCardSidebar.jsx
@@ -21,41 +21,69 @@ export const GroupCardSidebar = ({ groupData, joinGroup, leaveGroup }) => {
 		setUserData({ ...userData, groupsJoined: filteredGroup });
 	};
 
+	const background_image = {
+		backgroundImage: `url(${groupData?.avatar})`,
+		backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        maxWidth: '100%',
+        maxHeight: '100%'
+	};
+	// const [style, setStyle] = useState('');
+	const windowWidth = window.innerWidth;
+
+	// useEffect(() => {
+	// 	windowWidth > 1025
+    //   ? { ...background_image, backgroundImage: "none !important" }
+    //   : { ...background_image };
+	// 	// return () => {
+	// 	// 	cleanup
+	// 	// }
+	// }, [style])
+
+	const style =
+    windowWidth > 1025
+      ? { ...background_image, backgroundImage: "none !important" }
+      : { ...background_image };
+
+
+
 	return (
-		<div>
-			<Link to={`/groups/${groupData.id}/home`}>
-				<div>
-					<div className="listItemContent">
-						<div className="tag">
-							<img
-								src={groupData.avatar}
-								alt="group"
-								style={{
-									width: "100%",
-									maxHeight: "500px",
-									borderRadius: "8px",
-									objectFit: "contain",
-								}}
-							/>
-						</div>
-						<div className="sidebarItemText">
-							<h2 className="itemTitle"> {groupData.title}</h2>
-						</div>
-					</div>
-				</div>
-			</Link>
-			{groupData.groupOwner !== userData.name &&
-				groupMembers.includes(userData.name) && (
-					<button name="unjoin" onClick={handleClick}>
-						Unjoin Group
-					</button>
-				)}
-			{groupData.groupOwner !== userData.name &&
-				!groupMembers.includes(userData.name) && (
-					<button name="join" onClick={() => joinGroup(groupData.id)}>
-						Join Group
-					</button>
-				)}
-		</div>
-	);
+    <>
+      <div>
+        {groupData.groupOwner !== userData.name &&
+          groupMembers.includes(userData.name) && (
+            // <button name="unjoin" onClick={handleClick}>
+            //   Unjoin Group
+            // </button>
+            <i class="fas fa-backspace" onClick={handleClick}></i>
+          )}
+      </div>
+      <Link to={`/groups/${groupData.id}/home`}>
+        <div className="sidebar-list" style={{ ...style }}>
+          <div className="sidebar-image">
+            <img
+              src={groupData.avatar}
+              alt="group"
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </div>
+          <div className="sidebar-text">
+            <h2 className="itemTitle"> {groupData.title}</h2>
+          </div>
+        </div>
+      </Link>
+
+      {/* <div>
+        {groupData.groupOwner !== userData.name &&
+          !groupMembers.includes(userData.name) && (
+            <button name="join" onClick={() => joinGroup(groupData.id)}>
+              Join Group
+            </button>
+          )}
+      </div> */}
+    </>
+  );
 };
