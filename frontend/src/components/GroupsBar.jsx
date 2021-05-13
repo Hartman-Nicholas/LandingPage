d30aafc8678be4eed4cabdc586ec90a7f2e68905
nh-g <giangngohong@gmail.com>
2021-05-13 07:02:59 +0200

    remove ul, li

--- frontend/src/components/GroupsBar.jsx
+++ frontend/src/components/GroupsBar.jsx
@@ -45,18 +45,9 @@ export const GroupsBar = () => {
           <Link to="/groups">Join a new group +</Link>
         </button>
         <h2>joined :</h2>
-        <ul className="list">
-          <li className="listItem">
-            <span>{groupsJoined}</span>
-          </li>
-        </ul>
-
+            {groupsJoined}
         <h2>created :</h2>
-        <ul className="list">
-          <li className="listItem">
-            <span>{groupCreated}</span>
-          </li>
-        </ul>
+            {groupCreated}
         <button className="sidebarButtonBottom">
           <Link to="/groups/create">+ Create a group</Link>
         </button>
