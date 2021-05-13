8e34055f8824372c5c6b784c052c4f75fdcb3d40
nh-g <giangngohong@gmail.com>
2021-05-13 09:38:17 +0200

    remove switchable background style between responsive queries

--- frontend/src/pages/groups/GroupCardSidebar.jsx
+++ frontend/src/pages/groups/GroupCardSidebar.jsx
@@ -7,60 +7,59 @@ import { useState } from "react";
 import { userDataState } from "../../state/userDataState";
 
 export const GroupCardSidebar = ({ groupData, joinGroup, leaveGroup }) => {
-	// State
-	const [userData, setUserData] = useRecoilState(userDataState);
-	const [groupMembers] = useState(groupData.members);
+  // State
+  const [userData, setUserData] = useRecoilState(userDataState);
+  const [groupMembers] = useState(groupData.members);
 
-	// Constants
-	const handleClick = (e) => {
-		e.preventDefault();
-		leaveGroup(groupData.id);
-		let filteredGroup = userData.groupsJoined.filter(
-			(group) => group.id !== groupData.id
-		);
-		setUserData({ ...userData, groupsJoined: filteredGroup });
-	};
+  // Constants
+  const handleClick = (e) => {
+    e.preventDefault();
+    leaveGroup(groupData.id);
+    let filteredGroup = userData.groupsJoined.filter(
+      (group) => group.id !== groupData.id
+    );
+    setUserData({ ...userData, groupsJoined: filteredGroup });
+  };
 
-	const background_image = {
-		backgroundImage: `url(${groupData?.avatar})`,
-		backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        maxWidth: '100%',
-        maxHeight: '100%'
-	};
-	// const [style, setStyle] = useState('');
-	const windowWidth = window.innerWidth;
+  // const background_image = {
+  //   backgroundImage: `url(${groupData?.avatar})`,
+  //   backgroundPosition: "center",
+  //   backgroundSize: "cover",
+  //   backgroundRepeat: "no-repeat",
+  //   maxWidth: "100%",
+  //   maxHeight: "100%",
+  // };
+  // const [style, setStyle] = useState('');
+  // const windowWidth = window.innerWidth;
 
-	// useEffect(() => {
-	// 	windowWidth > 1025
-    //   ? { ...background_image, backgroundImage: "none !important" }
-    //   : { ...background_image };
-	// 	// return () => {
-	// 	// 	cleanup
-	// 	// }
-	// }, [style])
+  // useEffect(() => {
+  // 	windowWidth > 1025
+  //   ? { ...background_image, backgroundImage: "none !important" }
+  //   : { ...background_image };
+  // 	// return () => {
+  // 	// 	cleanup
+  // 	// }
+  // }, [style])
 
-	const style =
-    windowWidth > 1025
-      ? { ...background_image, backgroundImage: "none !important" }
-      : { ...background_image };
+  // const style =
+  //   windowWidth > 1025
+  //     ? { ...background_image, backgroundImage: "none !important" }
+  //     : { ...background_image };
 
-
-
-	return (
-    <>
-      <div>
+  return (
+    <div className="sidebar-list">
+      <div className="sidebar danger-btn">
         {groupData.groupOwner !== userData.name &&
           groupMembers.includes(userData.name) && (
             // <button name="unjoin" onClick={handleClick}>
             //   Unjoin Group
             // </button>
-            <i class="fas fa-backspace" onClick={handleClick}></i>
+            <i class="fas fa-sign-out-alt" onClick={handleClick}></i>
           )}
       </div>
       <Link to={`/groups/${groupData.id}/home`}>
-        <div className="sidebar-list" style={{ ...style }}>
+        {/* <div className="sidebar-group-list" style={{ ...style }}> */}
+        <div className="sidebar-group-list">
           <div className="sidebar-image">
             <img
               src={groupData.avatar}
@@ -84,6 +83,6 @@ export const GroupCardSidebar = ({ groupData, joinGroup, leaveGroup }) => {
             </button>
           )}
       </div> */}
-    </>
+    </div>
   );
 };
