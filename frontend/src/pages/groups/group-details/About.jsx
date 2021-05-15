639b7a4e1fd8fc2de9f5fa3832afd89dbc3a9b31
Nicholas Hartman <hartn001@gmail.com>
2021-05-15 08:49:44 +0200

    Added group styles

--- frontend/src/pages/groups/group-details/About.jsx
+++ frontend/src/pages/groups/group-details/About.jsx
@@ -4,30 +4,32 @@ import ReactTimeAgo from "react-time-ago";
 // Project files
 
 export const About = ({ data }) => {
-	// State
+  // State
 
-	// Constants
+  // Constants
 
-	const allDetails = data && (
-		<div>
-			<h3>{data.description}</h3>
-			<h3>rules: {data.rules}</h3>
-			<h4>admin: {data.groupOwner}</h4>
-			<h4>
-				Topics:{" "}
-				{data.topics.map((topic) => (
-					<p key={topic.id}>{topic}</p>
-				))}
-			</h4>
-			<p>
-				<ReactTimeAgo date={new Date(data.created)} locale="en-US" />
-			</p>
-		</div>
-	);
-	// Components
-	return (
-		<div>
-			{allDetails}
-		</div>
-	);
+  const allDetails = data && (
+    <div>
+      <div className="aboutPage">
+        <p>Description:</p>
+        <p>{data.description}</p>
+        <p>Rules:</p>
+        <p>{data.rules}</p>
+        <p>Topics:</p>
+        <p>
+          {data.topics.map((topic) => (
+            <p key={topic.id}>{topic}</p>
+          ))}
+        </p>
+        <p>Created:</p>
+        <p>
+          <ReactTimeAgo date={new Date(data.created)} locale="en-US" />
+        </p>
+        <p>Owner:</p>
+        <p>{data.groupOwner}</p>
+      </div>
+    </div>
+  );
+  // Components
+  return <div>{allDetails}</div>;
 };
